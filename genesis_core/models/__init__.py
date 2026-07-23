67f38ba3e40405f463d082805d81e78c481fe9ab
chaosweaver007 <chaosweaver007@gmail.com>
2026-07-22 22:48:09 -0400

    Add model adapter package

---
+++ genesis_core/models/__init__.py
@@ -0,0 +1,5 @@
+"""Provider-neutral model adapter contracts."""
+
+from .base import DevelopmentModelAdapter, ModelAdapter, ModelResponse
+
+__all__ = ["DevelopmentModelAdapter", "ModelAdapter", "ModelResponse"]
